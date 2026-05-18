da0a2c52b88a34f0d82165948850752db3831213
WebSterK <websterk.eleve@gmail.com>
2026-05-18 19:10:23 +0200

    licentie CC

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ICT Programmeren 2026 - Programmeerproject
+
+**Auteur:** Kaatje Meers  
+**Jaar:** 2026
+
+## Licentie
+
+Dit project valt onder de **Creative Commons Naamsvermelding-NietCommercieel-GelijkDelen 4.0 Internationaal** licentie (CC BY-NC-SA 4.0).
+
+[![CC BY-NC-SA 4.0](https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png)](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.nl)
+
+Zie het [LICENSE](LICENSE) bestand voor meer informatie.
--- footer.php
+++ footer.php
@@ -2,9 +2,15 @@
 <div class="container-fluid mt-2">
 
     <div class="mt-2 mb-2 p-2 bg-info text-dark text-center rounded">
-        
-        <p>Footer</p>
-        
+
+        <p>
+            &copy; 2026 Kaatje Meers &mdash;
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.nl" target="_blank" rel="noopener" class="text-dark">
+                <img src="https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png" alt="CC BY-NC-SA 4.0">
+                
+            </a>
+        </p>
+
     </div>
 
 </div>
